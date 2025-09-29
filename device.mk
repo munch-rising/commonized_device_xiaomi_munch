e70f5990f8c0ff163ad1cabfe047a10286c92bb4
Olzhas-Kdyr <kdiroljas@gmail.com>
2025-09-29 14:28:26 +0500

    munch: Set INIT_VENDOR_LIB via soong config

--- device.mk
+++ device.mk
@@ -32,6 +32,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
+# Libinit
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_munch)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
